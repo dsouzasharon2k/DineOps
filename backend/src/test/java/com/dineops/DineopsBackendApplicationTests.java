915f0918369e38e51f2b0e9dd956fcb201c9a24e
dsouzasharon2k <sjd_2000@yahoo.com>
2026-03-11 06:13:54 +0000

    docs: Add DineOps_SRS_v1_0

---
+++ backend/src/test/java/com/dineops/DineopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
